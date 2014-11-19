d2b7ac242dcc4533dbd78a218a724d1d96a69e4e
lytemar@yahoo.com <lytemar@yahoo.com>
2014-11-18 21:11:53 -0600

    finished HW 8 problems 2 thru 6

--- HW8_probs7_8.m
+++ HW8_probs7_8.m
@@ -1,7 +1,11 @@
-%  HW 8 problems 2 thru 6 
+%  HW 8 problems 7, 8 
 
 % Initialization
 clear ; close all; clc
+K = 10; % K-fold cross-validation
+N = 100; % Number of runs
+C = [0.0001, 0.001, 0.01, 0.1, 1];
+Q = 2; % degree of the kernel polynomial
 
 % set path to libsvm
 addpath('~/Documents/Matlab/libsvm-3.20/matlab/');
@@ -13,47 +17,8 @@ trainlabels = trainData(:,1);
 trainfeatures = trainData(:, 2:end);
 testlabels = testData(:,1);
 testfeatures = testData(:, 2:end);
-%[trainlabels, trainfeatures ]= libsvmread('features.train.txt');
 
-% Create training labels for each digit vs. all
-trainlabelsMatrix = zeros(length(trainlabels),10);
-
-for i=0:9
-    trainlabelsMatrix(:,i+1) = trainlabels;
-    trainlabelsMatrix(trainlabels==i, i+1) = 1;
-    trainlabelsMatrix(trainlabels~=i, i+1) = -1;  
-end;
-
-% Create index sequences for problems 2 and 3
-prob2seq = [0, 2, 4, 6, 8];
-prob3seq = [1, 3, 5, 7, 9];
-l2 = length(prob2seq);
-l3 = length(prob3seq);
-MSE2 = zeros(l2, 1);
-MSE3 = zeros(l3, 1);
-
-% train the svm classifier
-for i=1:l2;
-    model = svmtrain(trainlabelsMatrix(:,prob2seq(i)+1), trainfeatures, ...
-    '-t 1 -d 2 -r 1, -g 1 -c 0.01');
-    [predictedLabels, accuracy, ~] = svmpredict(trainlabelsMatrix(:,prob2seq(i)+1), ...
-        trainfeatures, model, '-q');
-    MSE2(i,1) = accuracy(2);
-end;
-
-for i=1:l3;
-    model = svmtrain(trainlabelsMatrix(:,prob3seq(i)+1), trainfeatures, ...
-    '-t 1 -d 2 -r 1 -g 1 -c 0.01');
-    [predictedLabels, accuracy, ~] = svmpredict(trainlabelsMatrix(:,prob3seq(i)+1), ...
-        trainfeatures, model, '-q');
-    MSE3(i,1) = accuracy(2);
-end;
-
-% Training error for problems 2 and 3
-MSE2
-MSE3
-
-%%%%%%%%%%%%%%%%% problem 5 and 6 - 1 vs. 5 classifier
+%%%%%%%%%%%%%%%%%1 vs. 5 classifier
 % Subset the data to include data with 1 or 5 only
 indicesTrain15 = find(trainlabels == 1 | trainlabels == 5);
 trainData15 = trainData(indicesTrain15, :);
@@ -70,12 +35,6 @@ testlabels15(testlabels15 == 5) = -1;
 trainlabels15(trainlabels15 == 1) = 1;
 trainlabels15(trainlabels15 == 5) = -1;
 
-% Create list of C and Q values
-C = [0.0001, 0.001, 0.01, 0.1, 1];
-Q = [2, 5];
-Ein56 = zeros(length(Q), length(C));
-Eout56 = zeros(length(Q), length(C));
-numSV5 = zeros(length(Q), length(C));
 
 for i = 1:length(Q)
     for j= 1:length(C)
